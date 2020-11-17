dc1207bfc818104b67c439917871d72d87530993
bcw11 <bryan.adams1001@gmail.com>
2020-11-17 14:30:36 -0800

    nTESTING

---
+++ node.hpp
@@ -0,0 +1,35 @@
+#ifndef NODE
+#define NODE
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Node{
+    public:
+        //data
+        string firstName;
+        string lastName;
+        float CGPA;
+        int reserachScore;
+        int applicationID;
+
+        //link
+        Node* link;
+
+        //constructors
+        Node(string firstName, string lastName, float CGPA, int researchScore, int applicationID);
+        Node();
+};
+typedef Node* NodePtr;
+
+
+
+
+
+
+
+
+
+#endif
--- student.hpp
+++ student.hpp
@@ -1,33 +1,19 @@
 #ifndef STUDENT
 #define STUDENT
 
-using namespace std; 
+
 #include <string> 
+#include "node.hpp"
+using namespace std; 
 
 class Student{
 	protected:
-		string firstName;
-		string lastName;
-		float CGPA;
-		int researchScore;
-		int applicationID;
-
+		NodePtr head;
+		NodePtr tail;
 	public:
 		//Constructors
 		Student(string firstName, string lastName, float CGPA, int researchScore, int applicationID);
 		Student();
-
-		//Member functions
-		string getFirstName();
-		string getLastName();
-		float  getCGPA();
-		int getResearchScore();
-		int getApplicationID();
-		void setFirstName(string firstName);
-		void setLastName(string lastName);
-		void setCGPA(float CGPA);
-		void setResearchScore(int researchScore);
-		void setApplicationID(int applicationID);
 
 		//Friend functions
 		friend int compareCGPA(Student student1, Student student2);
